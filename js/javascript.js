144acca98a7f449c04c6514727839e21d0f4862e
willchilders1 <63604107+willchilders1@users.noreply.github.com>
2020-12-03 10:03:55 -0500

    add initial data and updates to index.html to begin adding qing dyansty data

---
+++ js/javascript.js
@@ -0,0 +1,223 @@
+(function (){
+  		// initial Leaflet map options
+		const options = {
+			zoomSnap: .1,
+			// center: [40, -90], 
+			// zoom: 4,
+			zoomControl: false
+		}
+
+		// create Leaflet map and apply options
+		const map = L.map('map', options);
+		new L.control.zoom({
+			position: "bottomleft"
+		}).addTo(map)
+
+
+		// request a basemap tile layer and add to the map
+		L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
+			attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+		}).addTo(map);
+
+		// set global variables for map layer,
+		// mapped attribute, and normalizing attribute
+		let attributeValue = "HC01_VC04"; //Population 16 years and over - In labor force
+		const normValue = "HC01_VC03"; //EMPLOYMENT STATUS - Population 16 years and over
+
+		// create object to hold legend titles
+		const labels = {
+			"HC01_VC04": "Population in Labor Force",
+			"HC01_VC06": "Population Employed",
+			"HC01_VC07": "Population Unemployed",
+			"HC01_VC09": "Population not in Labor Force"
+		}
+
+		$.getJSON("data/ca_labor.json", function (data) {
+			// jQuery method uses AJAX request for the GeoJSON data
+			drawMap(data);
+		});
+
+		function drawMap(data) {
+			const counties = L.geoJson(data, {
+				// style counties with initial default path options
+				style: function (feature) {
+					return {
+						color: '#20282e',
+						weight: 2,
+						fillOpacity: .6,
+						fillColor: '#1f78b4'
+					};
+				},
+				// add hover/touch functionality to each feature layer
+				onEachFeature: function (feature, layer) {
+
+					// when mousing over a layer
+					layer.on('mouseover', function () {
+
+						// change the stroke color and bring that element to the front
+						layer.setStyle({
+							color: '#a4c639'
+						}).bringToFront();
+					});
+
+					// on mousing off layer
+					layer.on('mouseout', function () {
+
+						// reset the layer style to its original stroke color
+						layer.setStyle({
+							color: '#20282e'
+						});
+					});
+				}
+			}).addTo(map);
+			// fit the map's bounds and zoom level using the counties extent
+			map.fitBounds(counties.getBounds(), {
+				padding: [18, 18] // add padding around counties
+			});
+			updateMap(counties);
+			addUi(counties); // add the UI controls
+		}
+
+		function updateMap(counties) {
+			// you could log counties to console here to 
+			// verify the Leaflet layers object is not accessible
+			// and scoped within this function
+			console.log(counties);
+
+			// get the class breaks for the current data attribute
+			const breaks = getClassBreaks(counties);
+
+			// loop through each county layer to update the color and tooltip info
+			counties.eachLayer(function (layer) {
+
+				const props = layer.feature.properties;
+
+				// set the fill color of layer based on its normalized data value
+				layer.setStyle({
+					fillColor: getColor(props[attributeValue] /
+						props[normValue], breaks)
+				});
+
+				// assemble string sequence of info for tooltip (end line break with + operator)
+				let tooltipInfo = `<b>${props["NAME"]} County</b></br>
+            ${((props[attributeValue] / props[normValue]) * 100).toLocaleString()}%`
+
+				// bind a tooltip to layer with county-specific information
+				layer.bindTooltip(tooltipInfo, {
+					// sticky property so tooltip follows the mouse
+					sticky: true
+				});
+
+			});
+
+			// update the legend with the current data attribute information
+			addLegend(breaks);
+			updateLegend(breaks);
+		}
+
+		function getClassBreaks(counties) {
+
+			// create empty Array for storing values
+			const values = [];
+
+			// loop through all the counties
+			counties.eachLayer(function (layer) {
+				let value = layer.feature.properties[attributeValue] / layer.feature.properties[normValue];
+				values.push(value); // push the normalized value for each layer into the Array
+			});
+
+			// determine similar clusters
+			const clusters = ss.ckmeans(values, 5);
+
+			// create an array of the lowest value within each cluster
+			const breaks = clusters.map(function (cluster) {
+				return [cluster[0], cluster.pop()];
+			});
+
+			//return array of arrays, e.g., [[0.24,0.25], [0.26, 0.37], etc]
+			return breaks;
+		}
+
+		function getColor(d, breaks) {
+			// function accepts a single normalized data attribute value
+			// and uses a series of conditional statements to determine which
+			// which color value to return to return to the function caller
+
+			if (d <= breaks[0][1]) {
+				return '#f3ddc7';
+			} else if (d <= breaks[1][1]) {
+				return '#e1ac77';
+			} else if (d <= breaks[2][1]) {
+				return '#d68e47';
+			} else if (d <= breaks[3][1]) {
+				return '#c97b2d'
+			} else if (d <= breaks[4][1]) {
+				return '#995d22'
+			}
+		}
+
+		// Add legend to map
+		function addLegend(legend) {
+
+			// create a new Leaflet control object, and position it top left
+			const legendControl = L.control({
+				position: 'topleft'
+			});
+
+			// when the legend is added to the map
+			legendControl.onAdd = function () {
+
+				// select a div element with an id attribute of legend
+				const legend = L.DomUtil.get('legend');
+
+				// disable scroll and click/touch on map when on legend
+				L.DomEvent.disableScrollPropagation(legend);
+				L.DomEvent.disableClickPropagation(legend);
+
+				// return the selection to the method
+				return legend;
+
+			};
+
+			// add the empty legend div to the map
+			legendControl.addTo(map);
+
+			// select the legend, add a title, begin an unordered list and assign to a variable
+		}
+
+		function updateLegend(breaks) {
+			const legend = $('#legend').html(`<h5>${labels[attributeValue]}</h5>`);
+
+			// loop through the Array of classification break values
+			for (let i = 0; i <= breaks.length - 1; i++) {
+
+				let color = getColor(breaks[i][0], breaks);
+
+				legend.append(
+					`<span style="background:${color}"></span>
+		<label>${(breaks[i][0] * 100).toLocaleString()} &mdash;
+		${(breaks[i][1] * 100).toLocaleString()}%</label>`);
+			}
+		}
+
+		function addUi(counties) {
+			$('#dropdown-ui select').change(function () {
+				// code executed here when change event occurs
+				attributeValue = this.value;
+				// call updateMap function
+				updateMap(counties);
+			});
+			// create the slider control
+			var selectControl = L.control({
+				position: "topright"
+			});
+
+			// when control is added
+			selectControl.onAdd = function () {
+				// get the element with id attribute of ui-controls
+				return L.DomUtil.get("dropdown-ui");
+			};
+			// add the control to the map
+			selectControl.addTo(map);
+		}
+  })();
